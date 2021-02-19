056a1b7df9d39699b0bea2f089d2a9f0ae68e85e
mark0smith <24486728+mark0smith@users.noreply.github.com>
2021-02-19 18:03:50 +0800

    Update surge bilibili feed.js

--- surge bilibili feed.js
+++ surge bilibili feed.js
@@ -2,34 +2,31 @@ var body = $response.body;
 body=JSON.parse(body);
 
 function should_filter(element) {
-    // 不喜欢原因数量为1个时，判断为广告或者推广，进行屏蔽
+	// 不喜欢原因数量为1个时，判断为广告或者推广，进行屏蔽
+	if ("three_point" in element) {
+		if ('dislike_reasons' in element['three_point']) {
+			if (element['three_point']['dislike_reasons'].length == 1) {
+				return true;
+			}
+		}
+	}
 
-    if("three_point" in element){
-        if( 'dislike_reasons' in element['three_point'] ){
-            if(element['three_point']['dislike_reasons'].length == 1){
-        	return true;
-	    }
-        }
-    }
 
-    // 没有播放参数信息，或者视频时长小于1分钟的短视频，进行屏蔽
-    if(! "player_args" in element){
-        return true;
-    }
-	if( ! "duration" in element["player_args"] ){
-        return true;
-    }
-	if( element['player_args']['duration'] < 60 ){
-        return true;
-    }
-
-    return false;
-
+	// 播放参数信息中视频时长小于1分钟的短视频，进行屏蔽
+	if ("player_args" in element) {
+		if ("duration" in element["player_args"]) {
+			if (element['player_args']['duration'] < 60) {
+				return true
+			}
+		}
+	}
+	return false;
 }
-body["data"]["items"].forEach((element, index) => {
-    should_be_filterd = should_filter(element)
 
-    console.log(`${index} ${should_be_filterd}`);
+body["data"]["items"].forEach((element, index) => {
+	should_be_filterd = should_filter(element)
+
+	console.log(`${index} ${should_be_filterd}`);
 	if (should_be_filterd) {
 		body["data"]["items"].splice(index, 1);
 	}
