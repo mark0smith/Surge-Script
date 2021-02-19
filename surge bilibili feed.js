28e97288c2dd339088d408225e57fdb485f9fa8c
mark0smith <24486728+mark0smith@users.noreply.github.com>
2021-02-19 18:39:57 +0800

    Update surge bilibili feed.js

--- surge bilibili feed.js
+++ surge bilibili feed.js
@@ -7,7 +7,7 @@ function should_filter(element) {
 	if ("three_point" in element) {
 		if ('dislike_reasons' in element['three_point']) {
 			if (element['three_point']['dislike_reasons'].length == 1) {
-				return true;
+				return (true, "广告/推广");
 			}
 		}
 	}
@@ -15,7 +15,12 @@ function should_filter(element) {
 	// 屏蔽直播 	
 	if ("card_goto" in element) {
 		if (element['card_goto'] == 'live'){
-			return true;
+			return (true, "直播");
+		}
+	}
+	if ("goto" in element) {
+		if (element['goto'] == 'live'){
+			return (true, "直播");
 		}
 	}
 
@@ -23,17 +28,19 @@ function should_filter(element) {
 	if ("player_args" in element) {
 		if ("duration" in element["player_args"]) {
 			if (element['player_args']['duration'] < 60) {
-				return true
+				return (true, "短视频");
 			}
 		}
 	}
-	return false;
+	return (false,"");
 }
 var new_array = [];
 body["data"]["items"].forEach((element, index) => {
-	should_be_filterd = should_filter(element)
+	info = should_filter(element)
+	should_be_filterd = info[0]
+	filter_reason = info[1]
 
-	console.log(`${index} ${should_be_filterd}`);
+	console.log(`${index} ${should_be_filterd} ${filter_reason}`);
 	if (! should_be_filterd) {
 		new_array.push(element)
 	}
