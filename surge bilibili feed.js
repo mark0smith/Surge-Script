d1e19f9031a78a3ae5d457e5ab502f4f1518d4c5
mark0smith <24486728+mark0smith@users.noreply.github.com>
2021-02-19 18:20:45 +0800

    Update surge bilibili feed.js

--- surge bilibili feed.js
+++ surge bilibili feed.js
@@ -1,5 +1,6 @@
 var body = $response.body;
 body=JSON.parse(body);
+console.log(`原始响应内容数量： ${body["data"]["items"].length}`);
 
 function should_filter(element) {
 	// 不喜欢原因数量为1个时，判断为广告或者推广，进行屏蔽
@@ -22,15 +23,18 @@ function should_filter(element) {
 	}
 	return false;
 }
-
+var new_array = [];
 body["data"]["items"].forEach((element, index) => {
 	should_be_filterd = should_filter(element)
 
 	console.log(`${index} ${should_be_filterd}`);
-	if (should_be_filterd) {
-		body["data"]["items"].splice(index, 1);
+	if (! should_be_filterd) {
+		new_array.push(element)
 	}
 });
+body["data"]["items"] = new_array;
 
+console.log(`修改后响应内容数量： ${body["data"]["items"].length}`);
 body=JSON.stringify(body)
+
 $done({body})
